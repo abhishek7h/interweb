d8568671dc007525d45d16c4597c327343e00f00
Abhishek Hari <abhishek18h@gmail.com>
2024-05-02 16:37:05 +0530

    added filler images in projects for now

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,9 +7,9 @@ const Projects = () => {
         <Card
           href="https://github.com/abhishek7h/aniyiltemple"
           target="_blank"
-          className="max-w-sm w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110"
+          className="max-w-sm w-full h-auto rounded-lg duration-300 hover:scale-110"
           imgAlt="A screenshot of the landing page of aniyiltemple.org"
-          imgSrc="https://private-user-images.githubusercontent.com/87843823/327329630-2ab382bf-b559-4219-8966-cd0460cd00b4.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ2NDU3NjAsIm5iZiI6MTcxNDY0NTQ2MCwicGF0aCI6Ii84Nzg0MzgyMy8zMjczMjk2MzAtMmFiMzgyYmYtYjU1OS00MjE5LTg5NjYtY2QwNDYwY2QwMGI0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MDIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTAyVDEwMjQyMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWY0YmYwZmMwZDY4OGNiZDlkMTk2MDMyMzg3NDBiMGNjZjliNmViZGQzZGJhNTVkZTljM2YzZDEwOWI2OWQzZTgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.dpBtI3DxLSj2OVEwaRfMTcsinN1tddUhOJ0z6x0ZtD4"
+          imgSrc="https://scontent.fccj6-1.fna.fbcdn.net/v/t31.18172-8/415030_359448197462762_29979831_o.jpg?stp=dst-jpg_p720x720&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jz_6ryLyxX0Ab6C-tNH&_nc_ht=scontent.fccj6-1.fna&oh=00_AfDsTJu31wjhigEVI0urvpZ0qFiAiR5cTXe4fRoXwhTWiw&oe=6637A428"
         >
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Website for Aniyil Temple
@@ -23,7 +23,7 @@ const Projects = () => {
       <div className="flex flex-col justify-center items-center px-10 py-10">
         <Card
           href="https://github.com/abhishek7h/minecraft"
-          className="max-w-sm w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110"
+          className="max-w-sm w-full h-auto rounded-lg duration-300 hover:scale-110"
           imgAlt="A screenshot from my Minecraft clone that displays a structure made with stone blocks that reads 'HI'"
           imgSrc="https://camo.githubusercontent.com/94d687b50e1d1205e4e7c2b018b17b937df85cd9c528cc6d220bda3bc36b33f8/68747470733a2f2f7062732e7477696d672e636f6d2f6d656469612f464a33396e485f61554141553747343f666f726d61743d6a7067266e616d653d6c61726765"
         >
@@ -38,7 +38,7 @@ const Projects = () => {
       <div className="flex flex-col justify-center items-center px-10 py-10">
         <Card
           href="https://github.com/abhishek7h/python"
-          className="max-w-sm w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110"
+          className="max-w-sm w-full h-auto rounded-lg duration-300 hover:scale-110"
           imgAlt="A screenshot from my Minecraft clone that displays a structure made with stone blocks that reads 'HI'"
           imgSrc="https://thedailyguardian.com/wp-content/uploads/2024/02/chris-ried-ieic5Tq8YMk-unsplash-scaled.jpg"
         >
@@ -54,17 +54,17 @@ const Projects = () => {
       <div className="flex flex-col justify-center items-center px-10 py-10">
         <Card
           href="https://github.com/abhishek7h/faces"
-          className="max-w-sm w-full h-auto relative z-0 rounded-lg transition-all duration-300 hover:scale-110"
+          className="max-w-sm w-full h-auto rounded-lg duration-300 hover:scale-110max-w-sm w-full h-auto rounded-lg duration-300 hover:scale-110"
           imgAlt="A screenshot of me with a cyan square detecting my face."
-          imgSrc="https://private-user-images.githubusercontent.com/87843823/327399127-4d7eb7ba-b64c-4c36-92e1-57e0ea1afef5.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ2NDY0NTgsIm5iZiI6MTcxNDY0NjE1OCwicGF0aCI6Ii84Nzg0MzgyMy8zMjczOTkxMjctNGQ3ZWI3YmEtYjY0Yy00YzM2LTkyZTEtNTdlMGVhMWFmZWY1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MDIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTAyVDEwMzU1OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTNmM2UyOTU5YjJiN2QyZjk1YmM5MmQ3MmM1ZmQ2YzIxMmY5ZGVkMjhjNzYzOTcwNThhYTdlYTJhZGRkZDIyZTkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.psKorubU-KgXPzm9QGhxOJfRguzEW8LijRlesOJ80Qk"
+          imgSrc="https://res.cloudinary.com/practicaldev/image/fetch/s--_2v52RkY--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/7eafo61hjlv3k4jqg1h5.png"
         >
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Python Face Detection
           </h5>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Face Recognition software made with Python using OpenCV. Developed
-            as a project for my school's coding competition. I won first prize
-            for it.
+            Face detection software made with Python using OpenCV. Developed as
+            a project for my school's coding competition. I won first prize for
+            it.
           </p>
         </Card>
       </div>
